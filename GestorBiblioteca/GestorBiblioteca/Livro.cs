0315f682c5e3ecc5d73b53bcbc4e9ec72334f536
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2026-09-23 16:56:24 +0100

    Criei um projeto GestorBiblioteca.

---
+++ GestorBiblioteca/GestorBiblioteca/Livro.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Livro{
+    public string Título {  get; set; }
+    public string Autor { get; set; }
+    public string ISBN { get; set; }
+    public int AnoPublicacao { get; set; }
+    public string Genero { get; set; }
+}
---
+++ GestorBiblioteca/GestorBiblioteca/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class Program
+{
+    // --------------------- Variáveis ---------------------
+
+    public static List<Livro> livros = new List<Livro>();
+
+    // ---------------------- Métodos ----------------------
+
+    static void Main(string[] args)
+    {
+        // --------------------- Cabeçalho ---------------------
+
+        Console.WriteLine("====== Gestor de Biblioteca ======\n");
+        Console.WriteLine("1. Listar livros");
+        Console.WriteLine("2. Pesquisar livro");
+        Console.WriteLine("3. Adicionar livro");
+        Console.WriteLine("4. Requesitar livro");
+        Console.WriteLine("5. Devolver livro");
+        Console.WriteLine("6. Remover livro");
+        Console.WriteLine("0. Sair\n");
+        Console.Write("Selecione uma opção: ");
+        int opcao = int.Parse(Console.ReadLine());
+
+        // ---------------------- Opções ----------------------
+
+        switch (opcao)
+        {
+            case 1:
+                // Listar livros
+                ListarLivros();
+                break;
+            case 2:
+                // Pesquisar livro
+                break;
+            case 3:
+                // Adicionar livro
+                AdicionarLivro();
+                break;
+            case 4:
+                // Requisitar livro
+                break;
+            case 5:
+                // Devolver livro
+                break;
+            case 6:
+                // Remover livro
+                break;
+            case 0:
+                // Sair
+                break;
+            default:
+                // Outras opções
+                break;
+        }
+    }
+
+    // ------------------ 1. Listar livros ------------------
+
+    private static void ListarLivros()
+    {
+        Console.WriteLine("====== 1. Listar Livros ======\n");
+    }
+
+    // ----------------- 3. Adicionar livros -----------------
+
+    private static void AdicionarLivro()
+    {
+        Console.WriteLine("====== 3. Adicionar Livro ======\n");
+        //titulo, autor, isbn, ano de publicaçao, genero
+        Console.Write("Título: ");
+        string titulo = Console.ReadLine();
+        Console.Write("Autor: ");
+        string autor = Console.ReadLine();
+        Console.Write("ISBN: ");
+        string isbn = Console.ReadLine();
+        Console.Write("Ano de Publicação: ");
+        int anoPublicacao = int.Parse(Console.ReadLine());
+        Console.Write("Género: ");
+        string genero = Console.ReadLine();
+
+        Livro livroNovo = new Livro
+        {
+            Título = titulo,
+            Autor = autor,
+            ISBN = isbn,
+            AnoPublicacao = anoPublicacao,
+            Genero = genero
+        };
+
+        livros.Add(livroNovo);
+
+    }
+}
